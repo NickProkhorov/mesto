1c91c8b74e67e62577f7894c18e982fb87acd309
Nick Prokhorov <nickky87@yandex.ru>
2022-08-02 00:07:21 +0300

    Правки после ревью_1

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,12 @@
 let popupNode = document.querySelector('.popup');
 let editProfile = document.querySelector('.profile__edit-profile');
 let popupNodeClose = document.querySelector('.popup__closed');
-
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
-
 let popupForm = popupNode.querySelector('.popup__form');
+let inputName = popupForm.querySelector('.popup__item_type_name');
+let inputJob = popupForm.querySelector('.popup__item_type_job');
 
-let inputName = popupForm.querySelector('.popup__item_name');
-let inputJob = popupForm.querySelector('.popup__item_job');
-let saveProfile = popupForm.querySelector('.popup__button');
 
 function setPopupInputValue() { 
     inputName.value = profileName.textContent.trim();
@@ -25,30 +22,19 @@ function setProfileDesc() {
 function formSubmitHandler (evt) {
     evt.preventDefault();
     setProfileDesc();
+    popupClose();
 }
 
 function popupOpen() {
     setPopupInputValue();
     popupNode.classList.add('popup_opened');
-    console.log('включил');
 } 
 
 function popupClose() {
     popupNode.classList.remove('popup_opened');
-       console.log('выключил');
-    } 
-
-function setProfileDescClosePopup() {
-    setProfileDesc();
-    popupClose();
-    console.log('выключил');
-
-}    
+       
+} 
 
 editProfile.addEventListener ('click', popupOpen);
 popupNodeClose.addEventListener ('click', popupClose);
 popupForm.addEventListener('submit', formSubmitHandler);
-saveProfile.addEventListener('click', setProfileDescClosePopup);
-
-
-
